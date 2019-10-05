b54af803a316d7f026a03dde672d265ff7696692
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-06 01:59:17 +0200

    Added Data project with interfaces

---
+++ src/ABC/ABC.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using ABC.Core.Interfaces;
+using ABC.Data.Interfaces;
+using Entities.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly DbSet<TEntity> _entities;
+        private readonly IApplicationDbContext _context;
+
+        public GenericRepository(IApplicationDbContext dbContext)
+        {
+            _entities = dbContext.Set<TEntity>();
+            _context = dbContext;
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _entities.Where(e => e.IsDeleted == false).FirstOrDefault();
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _entities;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public void Insert(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            _entities.Add(entity);
+        }
+
+        public void Insert(IEnumerable<TEntity> entities)
+        {
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+            _entities.AddRange(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            entity.IsDeleted = true;
+        }
+
+        public void Delete(IEnumerable<TEntity> entities)
+        {
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+            entities.ToList().ForEach(e =>
+            {
+                e.IsDeleted = true;
+            });
+        }
+    }
+}
