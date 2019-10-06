24483daec02b00f164bd1874469017edaf78907f
Mustafa Saeed <mustafasaeed202019@gmail.com>
2019-10-06 03:34:07 +0200

    Added unit of work implementaion

---
+++ src/ABC/ABC.Data/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using ABC.Core.Interfaces;
+using ABC.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IApplicationDbContext _context;
+        public UnitOfWork(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public int Complete()
+        {
+            try
+            {
+                return _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
